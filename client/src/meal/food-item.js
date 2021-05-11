ff0c3db25371f4a221add8bb565614e543c07329
Kameron Meyer <normakster@gmail.com>
2021-05-11 14:23:29 -0500

    extracted Editable and ReadOnly

--- client/src/meal/food-item.js
+++ client/src/meal/food-item.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useReducer } from 'react';
+import { Rows, Editable, ReadOnly } from '../components/form-row.component'
 import { foodReducer } from './food-reducer'
 import api from '../api'
 
@@ -10,7 +11,12 @@ const initialState = {
     name: '',
     desc: '',
     kCal: '',
-  }
+  },
+  keys: [
+    { key: 'name', placeholder: 'Name', dispatch: 'NAME', type: 'text', },
+    { key: 'desc', placeholder: 'Desc', dispatch: 'DESC', type: 'text', },
+    { key: 'kCal', placeholder: 'kCal', dispatch: 'KCAL', type: 'text', },
+  ]
 }
 
 // Components
@@ -47,36 +53,35 @@ const FoodItem = ({ foodItem, foodsDispatch, ingrDispatch, inCache }) => {
     })
   }
 
-  return (
-    <div >
-    {
-      (isEditable)
-      ? <Editable food={food} foodDispatch={foodDispatch} handleSave={handleSave} >
-          <div className='btn btn-warning col' onClick={(e) => handleCancel(e)}>Cancel</div>
-          <div className='btn btn-danger col' onClick={(e) => handleRemove(e)}>Delete</div>
-        </Editable>
-      : <ReadOnly food={food} >
-          <div className='btn btn-danger' onClick={() => setIsEditable(true)} >Edit</div>
-          &nbsp; or &nbsp;
-          <div className='btn btn-warning' onClick={handleAddRemove} >{inCache ? 'X' : 'Add'}</div>
-        </ReadOnly>
-    }
-    </div>
-  )
-}
-
-const ReadOnly = ({ food, children }) => {
-  return (
-    <div className='row border rounded'>
-      <div className='col'>{food.name}</div>
-      <div className='col'>{food.desc}</div>
-      <div className='col'>{food.kCal}</div>
-      <div className='col'>
-        <div className='row'>
-          {children}
-        </div>
+  function editBtns() {
+    return (
+      <div className='row'>
+        <input type='submit' value='Save' onClick={() => handleSave(food)} className='btn btn-primary col' />
+        <div className='btn btn-warning col' onClick={(e) => handleCancel(e)}>Cancel</div>
+        <div className='btn btn-danger col' onClick={(e) => handleRemove(e)}>Delete</div>
       </div>
-    </div>
+    )
+  }
+
+  function roBtns() {
+    return (
+      <div className='row'>
+        <div className='btn btn-danger' onClick={() => setIsEditable(true)} >Edit</div>
+        &nbsp; or &nbsp;
+        <div className='btn btn-warning' onClick={handleAddRemove} >{inCache ? 'X' : 'Add'}</div>
+      </div>
+    )
+  }
+
+  return (
+    <Rows
+    item={food}
+    dispatch={foodDispatch}
+    keys={initialState.keys}
+    isEditable={isEditable}
+    editBtns={editBtns}
+    roBtns={roBtns}
+    />
   )
 }
 
@@ -91,83 +96,33 @@ const NewFood = ({ callback }) => {
     callback();
   }
 
-  function handleCancel(e) {
+  function handleCancel() {
     setIsEditable(false);
     callback();
   }
 
-  return (
-    <div>
-    {
-      (isEditable)
-      ? <Editable food={food} foodDispatch={foodDispatch} handleSave={handleSave} >
-          <div className='btn btn-primary col' onClick={(e) => handleCancel(e)}>Cancel</div>
-        </Editable>
-      : <ReadOnly food={food} >
-          <div className='btn btn-primary' onClick={() => setIsEditable(true)} >UnSaved</div>
-        </ReadOnly>
-    }
-    </div>
+  function editBtns() {
+    return (
+      <div className='row'>
+        <input type='submit' value='Save' onClick={() => handleSave(food)} className='btn btn-primary col' />
+        <div className='btn btn-primary col' onClick={() => handleCancel()}>Cancel</div>
+      </div>
+    )
+  }
 
-  )
-}
-
-const Editable = ({ food, foodDispatch, handleSave, children }) => {
-
-  function onSubmit(e) {
-    e.preventDefault();
-    console.log(food);
-    handleSave(food);
+  function roBtns() {
+    return <div className='btn btn-primary' onClick={() => setIsEditable(true)} >UnSaved</div>
   }
 
   return (
-      <form onSubmit={onSubmit} >
-        <div className='form-row'>
-          <div className='col-auto'>
-            <label className='sr-only' >Name</label>
-            <input
-                required
-                name='name'
-                type='text'
-                className='form-control mb-2'
-                value={food.name}
-                placeholder='Name'
-                onChange={(e)=>{foodDispatch({type:'NAME',payload:e.target.value})}}
-                />
-          </div>
-          <div className='col-auto'>
-            <label className='sr-only' >Desc.</label>
-            <input
-                required
-                name='desc'
-                type='text'
-                className='form-control'
-                value={food.desc}
-                placeholder='Desc.'
-                onChange={(e)=>{foodDispatch({type:'DESC',payload:e.target.value})}}
-                />
-          </div>
-          <div className='col-auto'>
-            <label className='sr-only' >kCal</label>
-            <input
-                required
-                name='kCal'
-                type='text'
-                className='form-control'
-                value={food.kCal}
-                placeholder='kCal'
-                onChange={(e)=>{foodDispatch({type:'KCAL',payload:e.target.value})}}
-                />
-          </div>
-          <div className='col-auto'>
-            <div className='row'>
-              <input type='submit' value='Save' className='btn btn-primary col' />
-              {children}
-            </div>
-          </div>
-        </div>
-        <div className=''>{food._id}</div>
-      </form>
+    <Rows
+    item={food}
+    dispatch={foodDispatch}
+    keys={initialState.keys}
+    isEditable={isEditable}
+    editBtns={editBtns}
+    roBtns={roBtns}
+    />
   )
 }
 
